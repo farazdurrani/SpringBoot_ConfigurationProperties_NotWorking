b853eba2d66efa5034282c1fcf47587c7295489c
Faraz Durrani <faraz.uic@gmail.com>
2019-07-09 10:13:58 -0500

    cleaned up this branch. Final Result -> Spring Boot doesn't support properties reload without redeploying the application

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,42 @@
 package com.example.demo;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
+	@Value("${envvar}")
+	private String envvar;
+	
+	@GetMapping("/printenv")
+	public String printEnv() {
+		return envvar;
+	}
+	
+	@Autowired
+	public Environment env;
+	
+	@PostMapping("setenv")
+	public String setEnv(@RequestBody Map<String, String> map) {
+		map.forEach((x,y)->{
+			System.setProperty(x, y);
+			envvar = y;
+		});
+		return "done";
 	}
 }
